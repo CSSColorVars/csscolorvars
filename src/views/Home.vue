3c5423a2f889194d16b0a175e24ae4586da5a0ca
Fernando Javier Averanga Aruquipa <nandes.ingsistemas@gmail.com>
2018-09-25 00:17:03 -0400

    algunos de limpieza

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,20 +2,17 @@
   <div class="ed-container full">
     <nds-sidebar></nds-sidebar>
     <nds-main></nds-main>
-    <!-- <nds-colorsbar></nds-colorsbar> -->
   </div>
 </template>
 
 <script>
 import NdsMain from '@/components/NdsMain.vue'
 import NdsSidebar from '@/components/NdsSidebar.vue'
-import NdsColorsbar from '@/components/NdsColorsbar.vue'
 export default {
   name: 'home',
   components: {
     NdsSidebar,
-    NdsMain,
-    NdsColorsbar
+    NdsMain
   }
 }
 </script>
