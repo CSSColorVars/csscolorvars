769ef77ef43e1547dab05ea1a3d60584acea85bb
Fernando Averanga <nandes.ingsistemas@gmail.com>
2018-11-28 18:29:56 -0400

    social media in proces

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -5,6 +5,9 @@
     <nds-pallete></nds-pallete>
     <nds-notification></nds-notification>
     <nds-getcode></nds-getcode>
+    <footer class="nds-footer">
+      <a href="https://twitter.com/2062nandes?ref_src=twsrc%5Etfw" class="twitter-follow-button" data-show-count="false">Follow @2062nandes</a>
+    </footer>
   </div>
 </template>
 
@@ -25,3 +28,14 @@ export default {
   }
 }
 </script>
+<style lang="scss">
+  @import 'src/sass/mixins.scss';
+  .nds-footer {
+    bottom: .5rem;;
+    @include from(l){
+      position: fixed;
+      left: 20%;
+      padding-left: 1rem;
+    }
+  }
+</style>
