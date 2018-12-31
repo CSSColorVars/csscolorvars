50867aecf5ffd3c9a5ca4625b998a0ebd055e75b
Fernando Javier Averanga Aruquipa <nandes.ingsistemas@gmail.com>
2018-12-31 00:15:12 -0400

    footer in progress, and fierros Sass locales full

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -5,8 +5,25 @@
     <nds-pallete></nds-pallete>
     <nds-notification></nds-notification>
     <nds-getcode></nds-getcode>
-    <footer class="nds-footer">
-      <a href="https://twitter.com/2062nandes?ref_src=twsrc%5Etfw" class="twitter-follow-button" data-show-count="false">Follow @2062nandes</a>
+    <footer class="nds-footer nds-container full">
+      <div class="nds-item l-20 nds-container center">
+        <!-- <div class="nds-item l-100 s-50 l-block">
+          <a href="">Guide</a> .
+          <a href="">API</a>
+        </div> -->
+        <div class="nds-item l-100 s-50 l-block">
+          <!-- Place this tag where you want the button to render. -->
+          <a class="github-button" href="https://github.com/CSSColorVars/csscolorvars" data-show-count="true" aria-label="Star ntkme/github-buttons on GitHub">Star</a>
+        </div>
+      </div>
+      <div class="nds-item l-80 copyright cross-end">
+        <p class="cross-center">
+          create by&nbsp;
+          <a href="https://twitter.com/2062nandes" target="_blank"><b>Fernando Javier Averanga Aruquipa</b></a>
+          &nbsp;·&nbsp;
+          <a href="https://twitter.com/2062nandes?ref_src=twsrc%5Etfw" class="twitter-follow-button" data-show-count="false">Follow @2062nandes</a>
+        </p>
+      </div>
     </footer>
   </div>
 </template>
@@ -33,10 +50,15 @@ export default {
   .nds-footer {
     bottom: 1rem;;
     @include from(l){
-      width: 20%;
       position: fixed;
       left: 0;
-      text-align: center;
+      bottom: 0;
     }
+  }
+  .center{
+    text-align: center;
+  }
+  .copyright{
+    padding-left: 1rem;
   }
 </style>
