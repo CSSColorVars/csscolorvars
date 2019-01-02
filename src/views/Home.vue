e6ff9fd118e7ea19ddefad2c781fd8ab999e9537
Fernando Javier Averanga Aruquipa <nandes.ingsistemas@gmail.com>
2019-01-01 23:45:54 -0400

    Revert "npm update"
    This reverts commit 0ef9e2a1cf65f7dac694828beefd58d516a142af.

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -6,24 +6,20 @@
     <nds-notification></nds-notification>
     <nds-getcode></nds-getcode>
     <footer class="nds-footer nds-container full">
-      <div class="nds-item l-20 menu-footer nds-container center">
-        <div class="nds-item l-block darkfrom-l">
-          &nbsp;·&nbsp;
-          <a href="guide" target="_blank">Guide</a>&nbsp;&nbsp;·&nbsp;
-          <a href="#" target="_blank">About</a>
-          <!-- &nbsp;&nbsp;·&nbsp; -->
-          <!-- <a href="#" target="_blank">API</a> -->
-        </div>
-        <div class="nds-item l-block">
+      <div class="nds-item l-20 nds-container center">
+        <!-- <div class="nds-item l-100 s-50 l-block">
+          <a href="">Guide</a> .
+          <a href="">API</a>
+        </div> -->
+        <div class="nds-item l-100 s-50 l-block">
           <!-- Place this tag where you want the button to render. -->
           <a class="github-button" href="https://github.com/CSSColorVars/csscolorvars" data-show-count="true" aria-label="Star ntkme/github-buttons on GitHub">Star</a>
         </div>
       </div>
       <div class="nds-item l-80 copyright cross-end">
-        <hr class="to-l">
-        <p class="cross-center main-center">
+        <p class="cross-center">
           create by&nbsp;
-          <a class="inline" href="https://twitter.com/2062nandes" target="_blank"><b>Fernando Javier Averanga Aruquipa</b></a>
+          <a href="https://twitter.com/2062nandes" target="_blank"><b>Fernando Javier Averanga Aruquipa</b></a>
           &nbsp;·&nbsp;
           <a href="https://twitter.com/2062nandes?ref_src=twsrc%5Etfw" class="twitter-follow-button" data-show-count="false">Follow @2062nandes</a>
         </p>
@@ -51,47 +47,8 @@ export default {
 </script>
 <style lang="scss">
   @import 'src/sass/mixins.scss';
-  .darkfrom-l{
-    color: rgba(0, 0, 0, 0.54);
-    a{
-      position: relative;
-      color: rgba(0, 0, 0, 0.54);
-      padding-bottom: 3px;
-      &:hover {
-        color: #303030;
-        &::before{
-        left: 0;
-        right: 0;
-        }
-      }
-      &::before {
-        content: "";
-        position: absolute;
-        left: 51%;
-        right: 51%;
-        bottom: 0;
-        background: #303030;
-        height: 3px;
-        transition-property: left, right;
-        transition-duration: 0.3s;
-        transition-timing-function: ease-out;
-      }
-    }
-    @include from(l){
-      color: #c4c4c4;
-      a{
-        color: #c4c4c4;
-        &:hover {
-          color: #FFF;
-        }
-        &::before {
-          background: #FFF;
-        }
-      }
-    }
-  }
   .nds-footer {
-    bottom: 1rem;
+    bottom: 1rem;;
     @include from(l){
       position: fixed;
       left: 0;
@@ -101,29 +58,7 @@ export default {
   .center{
     text-align: center;
   }
-  @include to(l){
-    .menu-footer.nds-item.nds-container{
-      padding-right: 80px;
-    }
-  }
   .copyright{
     padding-left: 1rem;
-    padding-right: 80px;
-    color: rgba(0, 0, 0, 0.54);
-    a{
-      color: rgba(0, 0, 0, 0.54);
-      &:hover{
-        color: #303030;
-      }
-    }
-    p{
-      @include to(l){
-        text-align: center;
-        width: 100%;
-      }
-    }
-    .inline{
-      display: inline-block;
-    }
   }
 </style>
